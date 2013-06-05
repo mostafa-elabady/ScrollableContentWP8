f89500805b1b87b7fda31658f055dd59163a23ca
Mostafa El-Abady <mostafa.elabady@gmail.com>
2013-06-05 07:29:37 +0200

    Added Project

---
+++ ScrollableContentWP8/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ScrollableContentWP8.Resources;
+
+namespace ScrollableContentWP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ScrollViewer_ManipulationDelta_1(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
+        {
+            
+            BottomGrid.Height -= e.DeltaManipulation.Translation.Y;
+        }
+
+
+    }
+}
